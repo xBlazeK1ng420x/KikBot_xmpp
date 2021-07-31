d068a454c9c1c6d79f97bffd1b7df796cef3da86
xBlazeK1ng420x <64936421+xBlazeK1ng420x@users.noreply.github.com>
2021-07-31 02:55:19 -0700

    Update permit.js

--- src/permit.js
+++ src/permit.js
@@ -1,1 +1,16 @@
 
+let jids = [
+    "skidlalo_wv0@talk.kik.com", "loggers_g35@talk.kik.com", "king",
+   ],
+    perm = (current) => {
+    for (let i = 0; i < jids.length; i++) {
+        const jid = jids[i];
+        if(current == jid){
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+module.exports = {jids: jids, perm: perm};
