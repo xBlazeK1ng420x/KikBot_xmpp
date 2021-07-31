e15004551bb8a28dd395c2939388bc19f46e18a5
xBlazeK1ng420x <64936421+xBlazeK1ng420x@users.noreply.github.com>
2021-07-31 03:14:58 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log("hello world :o");
+console.log("Rekkt_ByThe_Best");
 
 // define variables that reference elements on our page
 const theStanza = document.getElementById("theStanza");
